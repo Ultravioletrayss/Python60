7b43ebf709dcd94c055a07ad73e1acc6b0111ce8
Ultraviletrays <G250009@e.ntu.edu.sg>
2026-01-17 01:02:37 +0800

    feat: 添加数学计算和循环示例代码
    - 创建3.py文件实现1到10的累加计算功能
    - 创建4.py文件实现九九乘法表的打印功能
    - 修改main.py添加基本算术运算和条件判断示例
    - 在main.py中实现两个数字的加法运算
    - 添加数值比较的条件分支逻辑

---
+++ 3.py
@@ -0,0 +1,6 @@
+
+total=0
+for i in range(1,101):
+    print("i=",i,"end")
+    total+= i
+    print("total=",total,"end")
--- main.py
+++ main.py
@@ -11,6 +11,16 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    num1=2
+    num2=3
+    result=num1+num2
+    print(result)
+    if num1>num2:
+        print("num1 is greater than num2")
+    elif num1<num2:
+        print("num1 is less than num2")
+    else:
+        print("num1 is equal to num2")
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
